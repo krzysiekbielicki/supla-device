59e81a8e467805a89497a6ac97e3da814523579d
krzysiekbielicki <krzysiekbielicki@users.noreply.github.com>
2025-04-01 05:57:44 +0000

    deploy: fe7dd6ed74874adc90e6b504a3ce362dccf63034

---
+++ search/functions_d.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['removesensor_0',['removeSensor',['../classSupla_1_1Control_1_1ValveBase.html#a1219856ed53ce43581a10a5853110fab',1,'Supla::Control::ValveBase::removeSensor()'],['../classValveBase.html#a1219856ed53ce43581a10a5853110fab',1,'ValveBase::removeSensor()']]]
+];
--- search/searchdata.js
+++ search/searchdata.js
@@ -1,18 +1,27 @@
 var indexSectionsWithContent =
 {
-  0: "_abcdefghiklmnoprstuvw",
-  1: "_abcdefghiklmnoprstuvw"
+  0: "16_abcdefghiklmnoprstuvw~",
+  1: "_abcdefghiklmnoprstuvw",
+  2: "abcdfghilmnoprsv~",
+  3: "am",
+  4: "ps"
 };
 
 var indexSectionNames =
 {
   0: "all",
-  1: "classes"
+  1: "classes",
+  2: "functions",
+  3: "variables",
+  4: "pages"
 };
 
 var indexSectionLabels =
 {
   0: "All",
-  1: "Classes"
+  1: "Classes",
+  2: "Functions",
+  3: "Variables",
+  4: "Pages"
 };
 
